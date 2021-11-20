93c23dfd0cb405780a02b5db7807e6197263bbfc
filmon araya <faafadis@gmail.com>
2021-11-20 11:57:41 -0600

    feat: intital commit

---
+++ src/main/java/edu/miu/project/foodelivery/FoodeliveryApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.project.foodelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/project/foodelivery/FoodeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.project.foodelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
